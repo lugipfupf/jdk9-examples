826a32e98d5506920e9b90a728f50eed8976a85b
Beat Luginbühl <luginbuehl@puzzle.ch>
2017-09-08 16:51:37 +0200

    takeWhile() and dropWhile() for Streams

--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -1,22 +1,47 @@
+import java.util.stream.Stream;
+
 public class Streams {
 
     public static void main(String[] args) {
         new Streams();
     }
-    
+
     private Streams() {
-        
+        takeWhileExample();
+        dropWhileExample();
+        iterateExample();
+        ofNullableExample();
     }
-    
-    void takeWhileExample() {
-        
+
+    private void takeWhileExample() {
+        System.out.println("\ntakeWhile()");
+
+        Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .takeWhile(i -> i < 5)
+            .forEach(System.out::print);
+
+        System.out.println();
     }
-    
-    void dropWhileExample() {
-        
+
+    private void dropWhileExample() {
+        System.out.println("\ndropWhile()");
+
+        Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .dropWhile(i -> i < 5)
+            .forEach(System.out::print);
+
+        System.out.println();
     }
-    
-    void iterateExample() {
-        
+
+    private void iterateExample() {
+        System.out.println("\niterate()");
+
+        System.out.println();
+    }
+
+    private void ofNullableExample() {
+        System.out.println("\nofNullable");
+
+        System.out.println();
     }
 }
