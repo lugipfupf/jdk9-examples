cec2626f84282a983054e2a0d6f22b6a272c2397
Beat Luginbühl <luginbuehl@puzzle.ch>
2017-09-19 14:02:06 +0200

    Http2 showing old and new way

--- src/javakafi/ch/puzzle/javakafi/jdk9/Http2.java
+++ src/javakafi/ch/puzzle/javakafi/jdk9/Http2.java
@@ -1,22 +1,19 @@
 package ch.puzzle.javakafi.jdk9;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.KeyStore;
+import java.net.URL;
 import java.time.Duration;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
 
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
 
 public class Http2 {
+    private static final String URL = "http://transport.opendata.ch/v1/connections?from=Bern&to=z%C3%BCrich&datetime=2017-09-21T17%3A00";
+
     public static void main(String[] args) throws Exception {
         System.out.println("HTTP/2 Client interface with JDK 9-181");
 
@@ -28,22 +25,48 @@ public class Http2 {
         basicHttp2Request();
     }
 
-    private void basicHtttRequest() {
+    private void basicHtttRequest() throws Exception {
+        System.out.println("Do request the old way:");
 
+        URL obj = new URL(URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+
+        int responseCode = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(responseCode);
+        System.out.println(response.toString());
+        System.out.println();
     }
 
-    private void basicHttp2Request() throws URISyntaxException, java.io.IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI("http://transport.opendata.ch/v1/connections?from=Bern&to=z%C3%BCrich&datetime=2017-09-21T17%3A00"))
+    private void basicHttp2Request() throws Exception {
+        System.out.println("Do request with new http2 client:");
+
+        // build request
+        HttpRequest request = HttpRequest
+            .newBuilder()
+            .uri(new URI(URL))
             .timeout(Duration.ofSeconds(5))
             .GET()
             .build();
 
+        // build client and feed it the request we just created
+        HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandler.asString());
 
         // behold: lots of unformatted JSON will be thrown at you
         System.out.println(response.statusCode());
         System.out.println(response.body());
+        System.out.println();
     }
 }
